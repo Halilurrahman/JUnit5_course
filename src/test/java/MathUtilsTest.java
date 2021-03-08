97c91b9eaae3bad299dc6be40c5c786903250e35
Halil Bagci <halilbagci75@hotmail.com>
2021-03-08 12:12:56 +0100

    Add math utilities test class to check MathUtils class

---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class MathUtilsTest {
+
+//    @Test
+//    void add() {
+//    }
+    @Test
+    void test() {
+//        fail ("not yet implemented");
+        System.out.println("this test ran mwah");
+    }
+//
+//    @org.junit.jupiter.api.Test
+//    void subtract() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void multiply() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void divide() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void computeCircleArea() {
+//    }
+}
